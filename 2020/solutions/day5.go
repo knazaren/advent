d78749836f5dd28467b6d734fbfc20a3249b27fb
Kostya Nazarenko <knazaren@gmail.com>
2021-04-15 12:28:50 -0400

    added day5 wip

---
+++ 2020/solutions/day5.go
@@ -0,0 +1,10 @@
+package solutions
+
+import "github.com/stretchr/testify/assert"
+
+func findRowNColID(input string, minRow, maxRow, minCol, maxCol int64) int64 {
+	if input == "" {
+		assert.Equals()
+	}
+	return 0
+}
