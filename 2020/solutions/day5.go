0e1fba91ff1bb755ac9246d55216bcfaefea68e2
Kostya Nazarenko <knazarenko@ptc.com>
2023-12-03 20:24:36 -0500

    last change in dev5

--- 2020/solutions/day5.go
+++ 2020/solutions/day5.go
@@ -1,10 +1,5 @@
 package solutions
 
-import "github.com/stretchr/testify/assert"
-
 func findRowNColID(input string, minRow, maxRow, minCol, maxCol int64) int64 {
-	if input == "" {
-		assert.Equals()
-	}
 	return 0
 }
